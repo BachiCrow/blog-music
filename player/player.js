3048417ab6f1ec2d1f378aed807bd1774d388363
BachiCrow <littlecorvooscuro@gmail.com>
2026-07-07 18:45:12 -0300

    Update player.js

--- player/player.js
+++ player/player.js
@@ -1,39 +1,101 @@
-console.log("Player.js cargado correctamente");
-const playlist = [
+<script>
+
+const canciones=[
 
 {
-    title: "3 Koopa Kingz",
-    artist: "aNTOJE",
-    audio: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/antoje-3-koopa-kingz.mp3",
-    cover: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-antoje.png"
+titulo:"3 Koopa Kingz - aNTOJE",
+audio:"https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/antoje-3-koopa-kingz.mp3",
+portada:"https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-antoje.png"
 },
 
 {
-    title: "Ballora's Music Box",
-    artist: "FNAFN Sister Location",
-    audio: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/fnafsl-ballora-music-box.mp3",
-    cover: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-ballora.png"
+titulo:"Ballora's Music Box",
+audio:"https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/fnafsl-ballora-music-box.mp3",
+portada:"https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-ballora.png"
 },
 
 {
-    title: "Mice Circus",
-    artist: "Coraline",
-    audio: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/mice-circus.mp3",
-    cover: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-mice-circus.png"
+titulo:"Mice Circus - Coraline",
+audio:"https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/mice-circus.mp3",
+portada:"https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-mice-circus.png"
 },
 
 {
-    title: "Steampianist Thing Feat",
-    artist: "Remix TikTok",
-    audio: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/steampianist-thing-feat.mp3",
-    cover: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-steampianist.png"
+titulo:"Steampianist - Thing",
+audio:"https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/steampianist-thing-feat.mp3",
+portada:"https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-steampianist.png"
 },
 
 {
-    title: "Jester's Playground",
-    artist: "Creepy Circus Music",
-    audio: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/jester-creepy-circus-music.mp3",
-    cover: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-jester.png"
+titulo:"Jester Creepy Circus",
+audio:"https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/jester-creepy-circus-music.mp3",
+portada:"https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-jester.png"
 }
 
 ];
+
+let indice=0;
+
+const audio=document.getElementById("audio");
+const cover=document.getElementById("cover");
+const title=document.getElementById("song-title");
+const prevBtn=document.getElementById("prev-song");
+const nextBtn=document.getElementById("next-song");
+
+function cargarCancion(){
+
+audio.src=canciones[indice].audio;
+cover.src=canciones[indice].portada;
+title.textContent=canciones[indice].titulo;
+
+}
+
+audio.addEventListener("ended",()=>{
+
+indice++;
+
+if(indice>=canciones.length){
+
+indice=0;
+
+}
+
+cargarCancion();
+
+audio.play();
+
+});
+prevBtn.addEventListener("click",()=>{
+
+    indice--;
+
+    if(indice<0){
+
+        indice=canciones.length-1;
+
+    }
+
+    cargarCancion();
+
+    audio.play();
+
+});
+
+nextBtn.addEventListener("click",()=>{
+
+    indice++;
+
+    if(indice>=canciones.length){
+
+        indice=0;
+
+    }
+
+    cargarCancion();
+
+    audio.play();
+
+});
+cargarCancion();
+
+</script></div>  
