40008a7a3e4a519b761ba9be2606a740de3b7b77
BachiCrow <littlecorvooscuro@gmail.com>
2026-07-07 17:05:50 -0300

    Update player.js

--- player/player.js
+++ player/player.js
@@ -1,1 +1,38 @@
+const playlist = [
 
+{
+    title: "3 Koopa Kingz",
+    artist: "aNTOJE",
+    audio: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/antoje-3-koopa-kingz.mp3",
+    cover: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-antoje.png"
+},
+
+{
+    title: "Ballora's Music Box",
+    artist: "FNAFN Sister Location",
+    audio: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/fnafsl-ballora-music-box.mp3",
+    cover: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-ballora.png"
+},
+
+{
+    title: "Mice Circus",
+    artist: "Coraline",
+    audio: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/mice-circus.mp3",
+    cover: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-mice-circus.png"
+},
+
+{
+    title: "Steampianist Thing Feat",
+    artist: "Remix TikTok",
+    audio: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/steampianist-thing-feat.mp3",
+    cover: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-steampianist.png"
+},
+
+{
+    title: "Jester's Playground",
+    artist: "Creepy Circus Music",
+    audio: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/music/jester-creepy-circus-music.mp3",
+    cover: "https://raw.githubusercontent.com/BachiCrow/blog-music/main/covers/1-jester.png"
+}
+
+];
