43a8cc43c178b17fbab91565b957b7ec0c8737bb
BachiCrow <littlecorvooscuro@gmail.com>
2026-07-16 17:15:41 -0300

    Add star burst effect on pointer down event

---
+++ cursor/cursor.js
@@ -0,0 +1,36 @@
+document.addEventListener("pointerdown", createStarBurst);
+
+function createStarBurst(e){
+
+    const totalStars = 10;
+
+    for(let i = 0; i < totalStars; i++){
+
+        const star = document.createElement("div");
+
+        star.className = "click-star";
+        star.textContent = "⭐";
+
+        star.style.left = e.clientX + "px";
+        star.style.top = e.clientY + "px";
+
+        const angle = Math.random() * Math.PI * 2;
+        const distance = 25 + Math.random() * 45;
+
+        star.style.setProperty("--x",
+            Math.cos(angle) * distance + "px");
+
+        star.style.setProperty("--y",
+            Math.sin(angle) * distance + "px");
+
+        document.body.appendChild(star);
+
+        star.addEventListener("animationend", () =>{
+
+            star.remove();
+
+        });
+
+    }
+
+}
