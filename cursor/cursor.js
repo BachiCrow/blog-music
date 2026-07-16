b4da18a3bc2eeeac23fa3e7cc473ba1191f6501b
BachiCrow <littlecorvooscuro@gmail.com>
2026-07-16 17:27:04 -0300

    Fix event parameter name in createStarBurst function

--- cursor/cursor.js
+++ cursor/cursor.js
@@ -1,6 +1,18 @@
+/*======================================
+        CURSOR PERSONALIZADO
+======================================*/
+
+
+
+
+
+/*======================================
+        PARTÍCULAS
+======================================*/
+
 document.addEventListener("pointerdown", createStarBurst);
 
-function createStarBurst(e){
+function createStarBurst(event){
 
     const totalStars = 10;
 
@@ -9,23 +21,29 @@ function createStarBurst(e){
         const star = document.createElement("div");
 
         star.className = "click-star";
+
         star.textContent = "⭐";
 
-        star.style.left = e.clientX + "px";
-        star.style.top = e.clientY + "px";
+        star.style.left = event.clientX + "px";
+        star.style.top = event.clientY + "px";
 
         const angle = Math.random() * Math.PI * 2;
+
         const distance = 25 + Math.random() * 45;
 
-        star.style.setProperty("--x",
-            Math.cos(angle) * distance + "px");
+        star.style.setProperty(
+            "--x",
+            Math.cos(angle) * distance + "px"
+        );
 
-        star.style.setProperty("--y",
-            Math.sin(angle) * distance + "px");
+        star.style.setProperty(
+            "--y",
+            Math.sin(angle) * distance + "px"
+        );
 
         document.body.appendChild(star);
 
-        star.addEventListener("animationend", () =>{
+        star.addEventListener("animationend", ()=>{
 
             star.remove();
 
